d04c46a9b8665f90cfcd81fea87625ea8054d971
EriickHenriique <eriickhenriique.s.silva@gmail.com>
2025-08-18 16:32:05 -0300

    adicionando solução 2 no exercício 02

--- Aula_01/exercicio_02.py
+++ Aula_01/exercicio_02.py
@@ -1,6 +1,11 @@
 #Exercício 2
 
+#Solução 1:
 soma1 = int(input('Digite o primeiro número da soma: '))
 soma2 = int(input('Digite o segundo número da soma: '))
 resultado = soma1 + soma2
 print(f'O resultado da soma é: {resultado}')
+
+#Solução 2:
+
+print('A soma é igual a: ' + str(int(input('Digite o Primeiro Número: ')) + int(input('Digite o segundo número: '))))
