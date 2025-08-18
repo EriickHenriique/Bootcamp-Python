84e351816c7d454bfe85352a3ce142c3929fe3d4
EriickHenriique <eriickhenriique.s.silva@gmail.com>
2025-08-18 20:59:51 -0300

    desafio aula02 concluido

--- Aula_02/exercicios_aula02.py
+++ Aula_02/exercicios_aula02.py
@@ -141,3 +141,102 @@ def numdif():
     num2 = int(input('Digite um número: '))
     resultado = print(num1 != num2)
     return resultado
+
+#Exercício 21: Conversor de Temperatura
+# Escreva um programa que converta a temperatura de Celsius para Fahrenheit. 
+# O programa deve solicitar ao usuário a temperatura em Celsius e, utilizando try-except, 
+# garantir que a entrada seja numérica, tratando qualquer ValueError. 
+# Imprima o resultado em Fahrenheit ou uma mensagem de erro se a entrada não for válida.
+
+def ConversorTemperatura():
+    try:
+        graus = int(input('Qual grau você deseja converter? '))
+        resultado = (graus * 1.8) + 32        
+        return print(resultado)         
+    except ValueError:
+        print('Erro: o input fornecedo é diferente de um número')
+
+#Exercício 22: Verificador de Palíndromo
+#Crie um programa que verifica se uma palavra ou frase é um palíndromo
+#  (lê-se igualmente de trás para frente, desconsiderando espaços e pontuações).
+#  Utilize try-except para garantir que a entrada seja uma string.
+#  Dica: Utilize a função isinstance() para verificar o tipo da entrada.
+
+def Palindromo():
+    entrada = input("Digite uma palavra ou frase: ")
+    if isinstance(entrada, str):
+        formatado = entrada.replace(" ", "").lower()
+        if formatado == formatado[::-1]:
+            print("É um palíndromo.")
+        else:
+            print("Não é um palíndromo.")
+    else:
+        print("Entrada inválida. Por favor, digite uma palavra ou frase.");
+
+#Exercício 23: Calculadora Simples
+#Desenvolva uma calculadora simples que aceite duas entradas numéricas e um operador (+, -, *, /) 
+# do usuário. Use try-except para lidar com divisões por zero e entradas não numéricas.
+#  Utilize if-elif-else para realizar a operação matemática baseada no operador fornecido.
+#  Imprima o resultado ou uma mensagem de erro apropriada.
+
+def CalculadoraSimples():
+    try:
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+        operador = input("Digite o operador (+, -, *, /): ")
+        if operador == '+':
+            resultado = num1 + num2
+        elif operador == '-':
+            resultado = num1 - num2
+        elif operador == '*':
+            resultado = num1 * num2
+        elif operador == '/' and num2 != 0:
+            resultado = num1 / num2
+        else:
+            print("Operador inválido ou divisão por zero.")
+            return CalculadoraSimples()
+        print("Resultado:", resultado)
+    except ValueError:
+        print("Erro: Entrada inválida. Certifique-se de inserir números.")
+        return CalculadoraSimples()
+    
+#Exercício 24: Classificador de Números
+#Escreva um programa que solicite ao usuário para digitar um número.
+#  Utilize try-except para assegurar que a entrada seja numérica
+#  e utilize if-elif-else para classificar o número como "positivo", "negativo" ou "zero".
+#  Adicionalmente, identifique se o número é "par" ou "ímpar".
+
+def ValidadorNumerico():
+    try:
+        numero = int(input("Digite um número: "))
+        if numero > 0:
+            print("Positivo")
+        elif numero < 0:
+            print("Negativo")
+        else:
+            print("Zero")
+        if numero % 2 == 0:
+            print("Par")
+        else:
+            print("Ímpar")
+    except ValueError:
+        print("Por favor, digite um número inteiro válido.")
+
+#Crie um script que solicite ao usuário uma lista de números separados por vírgula.
+# O programa deve converter a string de entrada em uma lista de números inteiros.
+# Utilize try-except para tratar a conversão de cada número e validar que cada elemento da lista 
+# convertida é um inteiro. 
+# Se a conversão falhar ou um elemento não for um inteiro,
+# imprima uma mensagem de erro. Se a conversão for bem-sucedida para todos os elementos, 
+# imprima a lista de inteiros.
+
+def Validador():
+    lista = str(input('Digite numeros separados por virgula: ')).split(',')
+    try:
+        lista_vazia = [int(num.strip()) for num in lista]
+        for i in lista_vazia:
+            print(i)
+    except ValueError:
+        print('Erro, número não é um inteiro ou valor digitado errado')
+        return Validador()  
+
