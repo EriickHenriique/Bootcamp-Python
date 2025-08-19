0b206d4fa01ded8b79c5f5973941738408a19ffb
EriickHenriique <eriickhenriique.s.silva@gmail.com>
2025-08-19 15:00:52 -0300

    atualizando desafio

--- Aula_03/desafio_03.py
+++ Aula_03/desafio_03.py
@@ -1,9 +1,5 @@
 ## Integre na solução anterior um fluxo de While que repita o fluxo até que o usuário insira as informações corretas
 
-
-
-
-
 def CalculoBonus():
     nome = False
     salario = False
